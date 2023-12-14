3d41c0bc5e78cdf406b86e6e968bec2a773b45f2
BasmalahGad <101473200+BasmalahGad@users.noreply.github.com>
2023-12-14 18:54:00 +0200

    SRTF done

--- src/schedulerSystem.java
+++ src/schedulerSystem.java
@@ -15,16 +15,16 @@ public class schedulerSystem {
         contextSwitch = in.nextInt();
         for (int i = 0; i < numOfProcesses; i++) {
             Process process = new Process();
-            System.out.println("Enter the " + (i + 1) + " process name :");
+            //System.out.println("Enter the " + (i + 1) + " process name :");
             process.setName(in.next());
-            System.out.println("Enter the " + (i + 1) + " process color :");
-            process.setColor(in.next());
-            System.out.println("Enter the " + (i + 1) + " process arrival time  :");
+           /* System.out.println("Enter the " + (i + 1) + " process color :");
+            process.setColor(in.next());*/
+            //System.out.println("Enter the " + (i + 1) + " process arrival time  :");
             process.setArrivalTime(in.nextInt());
-            System.out.println("Enter the " + (i + 1) + " process burst time :");
+          //  System.out.println("Enter the " + (i + 1) + " process burst time :");
             process.setBurstTime(in.nextInt());
-            System.out.println("Enter the " + (i + 1) + " process priority number :");
-            process.setPriorityNumber(in.nextInt());
+            /*System.out.println("Enter the " + (i + 1) + " process priority number :");
+            process.setPriorityNumber(in.nextInt());*/
             // this part is related to the Ag algorithm
 //            int rand = (int) (Math.random() * 21);
 //            if(rand < 10){
@@ -34,29 +34,31 @@ public class schedulerSystem {
 //            }else{
 //                process.setAGFactory(10 + process.getArrivalTime() + process.getBurstTime());
 //            }
-            System.out.println(process.getName() + " " + process.getAGFactory());
+   /*         System.out.println(process.getName() + " " + process.getAGFactory());
 
             // test case given
             if (i == 0) process.setAGFactory(20);
             else if (i == 1) process.setAGFactory(17);
             else if (i == 2) process.setAGFactory(16);
             else if (i == 3) process.setAGFactory(43);
-
+*/
             // another test case
 //            if(i==0)process.setAGFactory(20);
 //            else if(i==1)process.setAGFactory(19);
 //            else if(i==2)process.setAGFactory(24);
 //            else if(i==3)process.setAGFactory(38);
 
-            process.setQuantumTime(roundRobin);
+            /*process.setQuantumTime(roundRobin);*/
             processes.add(process);
         }
 //        SJFScheduler sjfScheduler = new SJFScheduler(processes,contextSwitch);
 //        sjfScheduler.schedule();
-        AGAlgorithm ag = new AGAlgorithm(processes);
-        ag.buildAlgo();
+        /*AGAlgorithm ag = new AGAlgorithm(processes);
+        ag.buildAlgo();*/
 
 //        PriorityScheduler priorityScheduler = new PriorityScheduler(processes);
 //        priorityScheduler.schedule();
+        SRTFScheduler srtfScheduler  = new SRTFScheduler(processes);
+        srtfScheduler.schedule();
     }
 }
